f0c5c0813d222e4a39f2e6f2277d4a17710bb8f7
AnhVu-Josep <vulva0201@gmail.com>
2025-10-02 10:35:23 +0700

    Nop slide 4

---
+++ PS43932_LeVanAnhVu_Slide4/src/main/java/org/example/ps43932_levananhvu_slide4/Ps43932LeVanAnhVuSlide4Application.java
@@ -0,0 +1,13 @@
+package org.example.ps43932_levananhvu_slide4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps43932LeVanAnhVuSlide4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps43932LeVanAnhVuSlide4Application.class, args);
+	}
+
+}
---
+++ PS43932_LeVanAnhVu_Slide4/src/test/java/org/example/ps43932_levananhvu_slide4/Ps43932LeVanAnhVuSlide4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ps43932_levananhvu_slide4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ps43932LeVanAnhVuSlide4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
